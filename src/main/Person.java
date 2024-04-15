b4ea54cd0210c164a9f81d816eb3b0bf99597397
Angelinamathew <angelinamathew707@gmail.com>
2024-04-14 22:14:19 -0400

    Updated

---
+++ src/main/Person.java
@@ -0,0 +1,68 @@
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE,
+        FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+    Sex sex;
+    int age;
+
+    public Sex getSex() {
+        return this.sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void printPerson() {
+        // ...
+    }
+}
